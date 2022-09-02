2d4b4c24ac79e6f931d69b249aded7d42c3f7009
craljimenez <craljimenez@utp.edu.co>
2022-09-02 10:37:16 -0500

    add kernel Gaussian Delta, and change Experimentala_Covertype to this kernel

--- SVM_Krein/kernels.py
+++ SVM_Krein/kernels.py
@@ -1,7 +1,7 @@
 #%% Libraries
 from sklearn.metrics.pairwise import sigmoid_kernel
 from scipy.spatial.distance import cdist
-from numpy import maximum
+from numpy import maximum,exp
 #%%
 class tanh_kernel:
     def __init__(self,
@@ -20,4 +20,19 @@ class TL1:
     def __call__(self, X,Y):
         D = cdist(X,Y,'minkowski',p=1)
         K = maximum(1-D,0)
+        return K
+
+#%% Gaussian Delta
+class gaussian_delta:
+    def __init__(self, params_kernel=None):
+        self.params_kernel = params_kernel
+    def __call__(self,X,Y):
+        if self.params_kernel is None:
+            self.params_kernel = {'a':[-1,1],
+                                  'sigmas':[X.shape[1]/2, X.shape[1]/2+1e-6]
+                                  }
+        D = cdist(X,Y,'euclidean')
+        K = 0
+        for ai,si in zip(self.params_kernel['a'],self.params_kernel['sigmas']):
+            K += ai*exp(-0.5*(D*D)/(si**2))
         return K
--- Sensibility_IR_oversampling_Covertype.py
+++ Sensibility_IR_oversampling_Covertype.py
@@ -1,6 +1,6 @@
 #%% load libraries
 from SVM_Krein.estimators import SVMK
-from SVM_Krein.kernels import tanh_kernel,TL1
+from SVM_Krein.kernels import tanh_kernel,TL1,gaussian_delta
 
 from sklearn.model_selection import StratifiedKFold,GridSearchCV
 from sklearn.metrics import f1_score,balanced_accuracy_score,recall_score,classification_report
@@ -110,16 +110,13 @@ for IR in [1,2,3,4,5]:
         Xtrain,t_train = X[train_index],t[train_index]
         Xtest,t_test = X[test_index],t[test_index]
         
-        C_list = [0.0001,0.001,0.1,1,10,100]
+        C_list = [0.00001,0.0001,0.001,0.1,1,10,100]
         s0 = np.median(pdist(Xtrain))
         kernels = []
         gamma_list = [s for s in np.linspace(.1*s0,1.2*s0,5)]
-
-        gamma_list = [2**(s) for s in range(-6,7)]
-
-        for s in list(itertools.product(gamma_list,np.logspace(-2,2,5))):
-            kernels.append( tanh_kernel(gamma=s[0],coef0=s[1]) )
-        # kernels.append( TL1(p = 0.7*X.shape[1]) )
+        for sf in [1,1.5,2,3]:
+            for s in list(itertools.product(gamma_list,np.logspace(-2,2,5))):
+                kernels.append( gaussian_delta(params_kernel={'sigmas':s,'a':[1.0,-1.00001]} ) )
         params_grid = {'C': C_list,
                     'kernel': kernels
                     }
